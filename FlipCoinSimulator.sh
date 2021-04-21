ddef3344873cd933420a1f623e3df266f90ad7a3
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 22:54:04 +0530

    add a function to check if game is tie then check the differnce between them and find the Winners

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,30 +1,72 @@
-#!/bin/bash 
+#!/bin/bash
 echo "Welcome To the Flip Coins Simulation Program"
 
 Head=0
 Tail=1
 HC=0
 TC=0
+diff=0
+Flip_Coin ()
+{
+ 	FlipCoin=$((RANDOM%2))
+        if (( $FlipCoin == $Head ))
+        then
+                HC=$((HC+1))
+        else
+                TC=$((TC+1))
+        fi
+
+}
+
+Check_Diff ()
+{
+	if (( $HC > $TC ))
+	then
+		diff=$((HC - TC))
+	else
+		diff=$((TC - HC))
+	fi
+}
+
+print()
+{
+	Check_Diff
+	echo "difference between Head and Tail is $diff"
+        echo "Total Tail Wins $TC"
+        echo "Total Head Wins $HC"
+}
+
 while (( $HC < 21 && $TC < 21 ))
 do
-	FlipCoin=$((RANDOM%2))
-	if (( $FlipCoin == $Head ))
-	then
-		HC=$((HC+1))
-	else
-		TC=$((TC+1))
-	fi
+        Flip_Coin
 done
-echo "HeadCount is : $HC"
-echo "Tail Count is : $TC"
 
-if (( $HC == 21 ))
+
+if (( $HC > $TC ))
 then
-	echo "Head Won The Game by : $(($HC - $TC))"
-elif (( $TC == 21 ))
+	echo "Head Winner"
+	print
+elif (( $HC < $TC ))
 then
-	echo "Tail Won The Game by : $(($TC - $HC))"
-elif (( $HC == 21 && $TC == 21 ))
-then
+	echo "Tail Winner"
+	print
+else
 	echo "Game Is Tie"
+	print
+	while (($diff<2))
+	do
+		Flip_Coin
+		Check_Diff
+	done
+	echo "if while done"
+	if [ $HC > $TC ]
+	then
+	        echo "Head Winner "
+	        print
+	elif [ $HC < $TC ]
+	then
+        	echo "Tail Winner "
+        	print
+	fi
+
 fi
