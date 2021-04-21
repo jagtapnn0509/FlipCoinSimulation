9c0a6bbd2c22cba78f18cb2800d0e86b8a522f63
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 20:58:08 +0530

    add a function to show Head Or Tail Won How much Times

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,12 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome To the Flip Coins Simulation Program"
 
 Head=0
 Tail=1
-FlipCoin=$((RANDOM%2))
-if (( $FlipCoin == $Head ))
-then
-	echo "Win Head"
-else
-	echo "Win Tail"
-fi
+HC=0
+TC=0
+for (( i=0; i<10; i++ ))
+do
+
+	FlipCoin=$((RANDOM%2))
+	if (( $FlipCoin == $Head ))
+	then
+		HC=$((HC+1))
+
+	else
+		TC=$((TC+1))
+
+	fi
+done
+echo "Head  Won $HC  Times"
+echo "Tail  Won $TC  Times"
