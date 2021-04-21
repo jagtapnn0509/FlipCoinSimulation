4afd2c6d34950694e61f5877147a57ff94afcda0
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 20:30:53 +0530

    Add a function to Display Head Or Tail Will Win

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome To the Flip Coins Simulation Program"
+
+Head=0
+Tail=1
+FlipCoin=$((RANDOM%2))
+if (( $FlipCoin == $Head ))
+then
+	echo "Win Head"
+else
+	echo "Win Tail"
+fi
