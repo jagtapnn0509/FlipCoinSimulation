770d73c0c72996aeaedb54a939b1d7f88cbeafd9
jagtapnn0509 <jagtapnn05@gmail.com>
2021-04-21 21:18:36 +0530

    Add a function To check Head or Tail won 21 times And By how much Differnces

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,22 +1,30 @@
-#!/bin/bash
+#!/bin/bash 
 echo "Welcome To the Flip Coins Simulation Program"
 
 Head=0
 Tail=1
 HC=0
 TC=0
-for (( i=0; i<10; i++ ))
+while (( $HC < 21 && $TC < 21 ))
 do
-
 	FlipCoin=$((RANDOM%2))
 	if (( $FlipCoin == $Head ))
 	then
 		HC=$((HC+1))
-
 	else
 		TC=$((TC+1))
-
 	fi
 done
-echo "Head  Won $HC  Times"
-echo "Tail  Won $TC  Times"
+echo "HeadCount is : $HC"
+echo "Tail Count is : $TC"
+
+if (( $HC == 21 ))
+then
+	echo "Head Won The Game by : $(($HC - $TC))"
+elif (( $TC == 21 ))
+then
+	echo "Tail Won The Game by : $(($TC - $HC))"
+elif (( $HC == 21 && $TC == 21 ))
+then
+	echo "Game Is Tie"
+fi
